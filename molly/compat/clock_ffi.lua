3345f37119f6e9ffde8fd75b723e5c45f354434a
Sergey Bronnikov <estetus@gmail.com>
2026-09-23 09:46:49 +0000

    emmyrc: fix undefined-field
    The patch enables diagnostic "undefined-field" and fixes warnings
    generated by this diagnostic.
    
    - molly.log builds its methods dynamically, so the analyzer cannot
      infer them. Annotate the table as table<string, any>.
    - molly.compat.clock_ffi indexes an FFI array with [0], which the
      analyzer does not understand; annotate the buffers with `any`.
    - molly.utils.setenv() called ffi.errno().errstring(), but
      ffi.errno() returns a number that has no errstring() method. Use
      strerror() from libc instead.

--- molly/compat/clock_ffi.lua
+++ molly/compat/clock_ffi.lua
@@ -34,6 +34,7 @@ local CLOCK_BOOTTIME_ALARM = 9
 -- luacheck: pop
 
 function clock.sleep(time)
+    ---@type any
     local ts = assert(ffi.new("ts[?]", 1))
     ts[0].tv_sec = math.floor(time)
     ts[0].tv_nsec = math.floor((time % 1) * 10^9)
@@ -41,18 +42,21 @@ function clock.sleep(time)
 end
 
 function clock.monotonic()
+    ---@type any
     local ts = assert(ffi.new("ts[?]", 1))
     ffi.C.clock_gettime(CLOCK_MONOTONIC, ts)
     return tonumber(ts[0].tv_sec) + tonumber(ts[0].tv_nsec) / 10^9
 end
 
 function clock.monotonic64()
+    ---@type any
     local ts = assert(ffi.new("ts[?]", 1))
     ffi.C.clock_gettime(CLOCK_MONOTONIC, ts)
     return tonumber(ts[0].tv_sec * 10^9 + ts[0].tv_nsec)
 end
 
 function clock.proc()
+    ---@type any
     local ts = assert(ffi.new("ts[?]", 1))
     ffi.C.clock_gettime(CLOCK_PROCESS_CPUTIME_ID, ts)
     return tonumber(ts[0].tv_sec) + tonumber(ts[0].tv_nsec) / 10^9
